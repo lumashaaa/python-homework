ec80aaa05c6c4f6aff1ece680e5ad20eba6c46b5
Maria <masha_s1924@mail.ru>
2025-12-05 14:32:59 +0300

    Delete models directory

--- models/author.py
+++
@@ -1,7 +0,0 @@
-class Author:
-    def __init__(self, name: str, group: str) -> None:
-        self.name = name
-        self.group = group
-
-    def __str__(self) -> str:
-        return f"{self.name} ({self.group})"
